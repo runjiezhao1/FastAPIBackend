b34c0df961ab8f9ddae30c3624368c756c7c29e3
Runjie Zhao <runjiezhao8@gmail.com>
2024-04-18 13:19:41 -0700

    update readme and clean the code

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 ## Instruction
 1. clone this repo by ```git clone https://github.com/runjiezhao1/FastAPIBackend.git```<br />
 2. use ```pip install``` to download and install the required packages<br />
+3. If you want to run the server, please execute the command ```uvicorn main:app --reload --port 5000```. If you wan to run the tests, please execute the command ```pytest```
+
+## Comments
+1. Postman and sqlite studio can better help you to understand what happens if you create, delete or select the task.
+
+## Code Structure
+| -- .gitignore
+| -- README.md
+| -- curd.py
+| -- database.py
+| -- main.py
+| -- models.py
+| -- schemas.py
+| -- sql_app.db
+| -- test_main.py
--- schemas.py
+++ schemas.py
@@ -2,45 +2,6 @@ from typing import List, Optional
 
 from pydantic import BaseModel
 
-
-# Item 的基类,表示创建和查询 Item 时共有的属性
-class ItemBase(BaseModel):
-    title: str
-    description: Optional[str] = None
-
-
-# 创建 Item 时的 Model
-class ItemCreate(ItemBase):
-    pass
-
-
-# 查询 Item 时的 Model
-class Item(ItemBase):
-    id: int
-    owner_id: int
-
-    # 向 Pydantic 提供配置
-    class Config:
-        #  orm_mode 会告诉 Pydantic 模型读取数据，即使它不是字典，而是 ORM 模型（或任何其他具有属性的任意对象）
-        orm_mode = True
-
-
-class UserBase(BaseModel):
-    email: str
-
-
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
-    is_active: bool
-    items: List[Item] = []
-
-    class Config:
-        orm_mode = True
-
 class TempOutput(BaseModel):
     id: int
     name: str
