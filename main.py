35c68d75aafd5a2ca3884ad8c1e1f5a55793cb0a
Runjie Zhao <runjiezhao8@gmail.com>
2024-04-18 06:37:49 -0700

    udpated create, retrieve and del apis

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, Depends, HTTPException, status, Path, Query, Body
 from database import SessionLocal, engine
 from sqlalchemy.orm import Session
 import schemas
-from models import Temp
-from schemas import User, UserCreate, ItemCreate, Item, TempCreate
+from models import Temp, EventEntity, EventUsers
+from schemas import User, UserCreate, ItemCreate, Item, TempCreate, EventEntityCreate
 import curd
 import sys
 app = FastAPI()
@@ -43,3 +43,52 @@ async def create_user(user: TempCreate, db: Session = Depends(get_db)):
     db.refresh(newTemp)
     #curd.create_names(db)
     return newTemp
+
+#create new tasks
+@app.post("/tasks")
+async def create_task(data: EventEntityCreate, db: Session = Depends(get_db)):
+    try:
+        newEventEntity = EventEntity(title=data.title, description=data.description, 
+                                    status=data.status, createdAt=data.createdAt, updatedAt=data.updatedAt,
+                                    startTime=data.startTime, endTime=data.endTime)
+        db.add(newEventEntity)
+        db.commit()
+        db.refresh(newEventEntity)
+    except:
+        return {"message":"Cannot add new task. Please check your input json"}
+    for i in data.userId:
+        try:
+            newEventUsers = EventUsers(eventId=newEventEntity.id, userId=i)
+            db.add(newEventUsers)
+            db.commit()
+            db.refresh(newEventUsers)
+        except:
+            return {"message":"Cannot add new related users to this task. Please check your input json"}
+    return {"message":"add tasks success"}
+
+#get task by id
+@app.get("/task/{task_id}")
+async def get_task(task_id: int, db: Session = Depends(get_db)):
+    try:
+        result = db.query(EventEntity).filter(EventEntity.id == task_id).first()
+    except:
+        return {"message":"Cannot get the task with specified id. Please check your input"}
+    return result
+
+#remove task by id
+@app.post("/remove_task/{task_id}")
+async def remove_task(task_id: int, db: Session = Depends(get_db)):
+    eventUsers = db.query(EventUsers).filter(EventUsers.eventId == task_id).all()
+    for i in eventUsers:
+        db.delete(i)
+        db.commit()
+    task = db.query(EventEntity).filter(EventEntity.id == task_id).all()
+    for i in task:
+        db.delete(i)
+        db.commit()
+    #delEventUsers = EventUsers(eventId=result.)
+    #db.delete(result)
+    #db.commit()
+    #result = db.query(EventUsers).filter(EventUsers.eventId == task_id).first()
+    #db.delete(EventUsers)
+    return {"message":"delete successful"}
