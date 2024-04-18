3e9765d2edfc705867873357fc346c2415785514
Runjie Zhao <runjiezhao8@gmail.com>
2024-04-18 13:08:42 -0700

    add more tests

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # FastAPIBackend
 
+## Instruction
--- main.py
+++ main.py
@@ -15,35 +15,10 @@ def get_db():
         yield db
     finally:
         db.close()
-
+#initial page used for testing
 @app.get("/")
 async def root():
     return {"message" : "Hello123"}
-
-# 创建用户
-# @app.post("/users", response_model=User)
-# async def create_user(user: UserCreate, db: Session = Depends(get_db)):
-#     # 1、先查询用户是否有存在
-#     db_user = curd.get_user_by_email(db, user.email)
-#     if db_user:
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="user 已存在")
-#     res_user = curd.create_user(db, user)
-#     return res_user
-
-# 根据 user_id 获取用户
-@app.get("/user/{user_name}",response_model=list[schemas.TempOutput])
-async def get_user(user_name: str, db: Session = Depends(get_db)):
-    #return {"message" : "user_name"}
-    return curd.get_names(db)
-
-@app.post("/users")
-async def create_user(user: TempCreate, db: Session = Depends(get_db)):
-    newTemp = Temp(name=user.name)
-    db.add(newTemp)
-    db.commit()
-    db.refresh(newTemp)
-    #curd.create_names(db)
-    return newTemp
 
 #create new tasks
 @app.post("/tasks")
